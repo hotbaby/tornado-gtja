f5209811aac2f42ab98e1184a89724de4d82a694
hotbaby <mengyy_linux@163.com>
2015-11-02 15:41:53 +0800

    Add conf file.

--- gtja/main.py
+++ gtja/main.py
@@ -1,5 +1,9 @@
+
+import json
 import os
-import tornado
+import tornado.web
+import tornado.httpserver
+from pymongo import Connection
 
 import conf
     
@@ -8,6 +12,7 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", HomeHandler),
+            (r"/report/abstract", AbstractHandler),
         ]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
@@ -17,14 +22,40 @@ class Application(tornado.web.Application):
             debug=True,
         )
         super(Application, self).__init__(handlers, **settings)
+        connection = Connection(conf.MONGODB_SERVER, conf.MONGODB_PORT)
+        self.db = connection[conf.MONGODB_DB]
 
 
 class BaseHandler(tornado.web.RequestHandler):
-    pass
+    
+    @property
+    def db(self):
+        return self.application.db
+
 
 class HomeHandler(BaseHandler):
     def get(self):
-        self.write("Welcome to gtja report website")
+        self.write("Welcome to gtja report website.")
+
+
+class AbstractHandler(BaseHandler):
+    
+    @property
+    def collection(self):
+        return self.db[conf.MONGODB_COLLECTION_REPORT_ABSTRACT]
+    
+    def get(self):
+        item = self.collection.find_one({"url":"http://www.gtja.com/fyInfo/contentForJunhong.jsp?id=694164"})
+        if item is None:
+            result = {}
+        else:
+            result = {
+                "title":item["title"],
+                "date":str(item["date"]),
+                "abstract":item["abstract"],
+                "url":item["url"],
+            }
+        self.write(json.dumps(result))
 
 
 def make_app():
